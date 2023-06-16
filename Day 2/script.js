5c4e332b298c3d91b4c8afc8f406e082fa33b747
Elkmar <dimitri.jeangerard@gmail.com>
2023-06-16 09:42:06 +0100

    Personal solution to day 2 - part 2

--- Day 2/script.js
+++ Day 2/script.js
@@ -27,5 +27,45 @@ const solvePuzzle1 = (file) => {
     });
 };
 
-solvePuzzle1('./puzzle.txt');
+const solvePuzzle2 = (file) => {
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const puzzle = data.toString();
+            const puzzleArray = puzzle.split(/\r?\n/);
+            const puzzleArrayCleaned = puzzleArray.map((item) => {
+                let dimensions = item.split('x');
+                return dimensions.map(Number);
+            });
 
+            answer = puzzleArrayCleaned.reduce((acc, item) => {
+                let l = item[0];
+                let w = item[1];
+                let h = item[2];
+                let volume = l * w * h;
+                let smallestSide = Infinity;
+                let secondSmallestSide = Infinity;
+
+                for (let dimension of item) {
+                    if (dimension < smallestSide) {
+                        secondSmallestSide = smallestSide;
+                        smallestSide = dimension;
+                    } else if (dimension < secondSmallestSide) {
+                        secondSmallestSide = dimension;
+                    }
+                }
+
+                let bow = (smallestSide + secondSmallestSide) * 2;
+                let total = volume + bow;
+
+                return acc += total;
+            }, 0);
+
+            console.log("Part 2 ", answer);
+        }
+    });
+};
+
+solvePuzzle1('./puzzle.txt');
+solvePuzzle2('./puzzle.txt');
