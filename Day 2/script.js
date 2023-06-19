cd613d6d808822f71f6d5029c3514840917dcbad
Elkmar <dimitri.jeangerard@gmail.com>
2023-06-19 22:55:50 +0100

    updated in synchronous function

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,71 +1,58 @@
 const fs = require('fs');
 
-const solvePuzzle1 = (file) => {
-    fs.readFile(file, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const puzzle = data.toString();
-            const puzzleArray = puzzle.split(/\r?\n/);
-            const puzzleArrayCleaned = puzzleArray.map((item) => {
-                let dimensions = item.split('x');
-                return dimensions.map(Number);
-            });
+const data = fs.readFileSync("puzzle.txt");
+const puzzle = data.toString();
+const puzzleArray = puzzle.split(/\r?\n/);
 
-            answer = puzzleArrayCleaned.reduce((acc, item) => {
-                let l = item[0];
-                let w = item[1];
-                let h = item[2];
-                let smallestSide = Math.min(l*w, w*h, h*l);
-                let surfaceArea = 2*l*w + 2*w*h + 2*h*l;
-                let total = surfaceArea + smallestSide;
-                return acc += total;
-            }, 0);
-
-            console.log("Part 1 ", answer);
-        }
+const solvePuzzle1 = () => {
+    const puzzleArray = puzzle.split(/\r?\n/);
+    const puzzleArrayCleaned = puzzleArray.map((item) => {
+        let dimensions = item.split('x');
+        return dimensions.map(Number);
     });
-};
 
-const solvePuzzle2 = (file) => {
-    fs.readFile(file, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const puzzle = data.toString();
-            const puzzleArray = puzzle.split(/\r?\n/);
-            const puzzleArrayCleaned = puzzleArray.map((item) => {
-                let dimensions = item.split('x');
-                return dimensions.map(Number);
-            });
+    let answer = puzzleArrayCleaned.reduce((acc, item) => {
+        let l = item[0];
+        let w = item[1];
+        let h = item[2];
+        let smallestSide = Math.min(l*w, w*h, h*l);
+        let surfaceArea = 2*l*w + 2*w*h + 2*h*l;
+        let total = surfaceArea + smallestSide;
+        return acc += total;
+    }, 0);
+    return answer;
+}
 
-            answer = puzzleArrayCleaned.reduce((acc, item) => {
-                let l = item[0];
-                let w = item[1];
-                let h = item[2];
-                let volume = l * w * h;
-                let smallestSide = Infinity;
-                let secondSmallestSide = Infinity;
-
-                for (let dimension of item) {
-                    if (dimension < smallestSide) {
-                        secondSmallestSide = smallestSide;
-                        smallestSide = dimension;
-                    } else if (dimension < secondSmallestSide) {
-                        secondSmallestSide = dimension;
-                    }
-                }
-
-                let bow = (smallestSide + secondSmallestSide) * 2;
-                let total = volume + bow;
-
-                return acc += total;
-            }, 0);
-
-            console.log("Part 2 ", answer);
-        }
+const solvePuzzle2 = () => {
+    const puzzleArrayCleaned = puzzleArray.map((item) => {
+        let dimensions = item.split('x');
+        return dimensions.map(Number);
     });
-};
 
-solvePuzzle1('./puzzle.txt');
-solvePuzzle2('./puzzle.txt');
+    let answer = puzzleArrayCleaned.reduce((acc, item) => {
+        let l = item[0];
+        let w = item[1];
+        let h = item[2];
+        let volume = l * w * h;
+        let smallestSide = Infinity;
+        let secondSmallestSide = Infinity;
+
+        for (let dimension of item) {
+            if (dimension < smallestSide) {
+                secondSmallestSide = smallestSide;
+                smallestSide = dimension;
+            } else if (dimension < secondSmallestSide) {
+                secondSmallestSide = dimension;
+            }
+        }
+
+        let bow = (smallestSide + secondSmallestSide) * 2;
+        let total = volume + bow;
+
+        return acc += total;
+    }, 0);
+    return answer;
+}
+
+console.log('Part 1 : ', solvePuzzle1());
+console.log('Part 2 : ', solvePuzzle2());
