3bb7d558756784cd39d6131c4bcc21177408cd77
Elkmar <dimitri.jeangerard@gmail.com>
2023-06-19 23:06:56 +0100

    updated in synchronous function

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,58 +1,42 @@
 const fs = require('fs');
 
-const solvePuzzle1 = (file) => {
-    console.time('challenge 1');
-    fs.readFile(file, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const puzzle = data.toString();
-            let floor = 0;
-            let index = 0;
-            let found = false;
-    
-            for (let char of puzzle) {
-                if ( char === "(") {
-                    floor += 1;
-                } else if (char ===")") {
-                    floor -= 1;
-                }
-                index += 1;
-            }
-            console.log("Santa is at floor ", floor);
-            console.timeEnd('challenge 1');
+const data = fs.readFileSync("puzzle.txt");
+const puzzle = data.toString();
+
+const solvePuzzle1 = () => {
+    let floor = 0;
+    let index = 0;
+
+    for (let char of puzzle) {
+        if ( char === "(") {
+            floor += 1;
+        } else if (char ===")") {
+            floor -= 1;
         }
-    });
-};
+        index += 1;
+    }
+    return floor;
+}
 
-const solvePuzzle2 = (file) => {
-    console.time('challenge 2');
-    fs.readFile(file, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const puzzle = data.toString();
-            let floor = 0;
-            let index = 0;
+const solvePuzzle2 = () => {
+    let floor = 0;
+    let index = 0;
 
-            for (let char of puzzle) {
-                if ( char === "(") {
-                    floor += 1;
-                } else if (char ===")") {
-                    floor -= 1;
-                }
-                
-                index += 1;
-
-                if (floor === -1) {
-                    console.log('Basement entered at position: ', index);
-                    console.timeEnd('challenge 2');
-                    break;
-                }
-            }
+    for (let char of puzzle) {
+        if ( char === "(") {
+            floor += 1;
+        } else if (char ===")") {
+            floor -= 1;
         }
-    });
-};
+        
+        index += 1;
 
-solvePuzzle1('./puzzle.txt');
-solvePuzzle2('./puzzle.txt');
+        if (floor === -1) {
+            return index;
+        }
+    }
+}
+
+
+console.log('Part 1 : ', solvePuzzle1());
+console.log('Part 2 : ', solvePuzzle2());
