2ac8f35079aeef930b9f7c4be807c77a5406e379
Elkmar <dimitri.jeangerard@gmail.com>
2023-06-20 10:00:03 +0100

    Solution Part 2

--- Day 3/script.js
+++ Day 3/script.js
@@ -2,28 +2,28 @@ const fs = require('fs');
 const data = fs.readFileSync('puzzle.txt');
 const puzzle = data.toString();
 
+const calculateNewPosition = (input, currentPosition) => {
+    let newPosition = [...currentPosition]
+    switch (input) {
+        case 'v' :
+            newPosition[1] -= 1;
+            break;
+        case '>' :
+            newPosition[0] += 1;
+            break;
+        case '^' :
+            newPosition[1] += 1;
+            break;
+        case '<' :
+            newPosition[0] -= 1;
+            break;
+    }
+    return newPosition;
+}
+
 const solvePuzzle1 = () => {
     let positionsVisited = [[0, 0]];
     let currentPosition = [0, 0];
-
-    const calculateNewPosition = (input, currentPosition) => {
-        let newPosition = [...currentPosition]
-        switch (input) {
-            case 'v' :
-                newPosition[1] -= 1;
-                break;
-            case '>' :
-                newPosition[0] += 1;
-                break;
-            case '^' :
-                newPosition[1] += 1;
-                break;
-            case '<' :
-                newPosition[0] -= 1;
-                break;
-        }
-        return newPosition;
-    }
 
     const arrayIncluded = (arrayCompared, setOfArray) => {
         for (let array of setOfArray) {
@@ -43,4 +43,35 @@ const solvePuzzle1 = () => {
     return positionsVisited.length;
 }
 
-console.log('Part 1 :', solvePuzzle1());
+const solvePuzzle2 = () => {
+    let positionsVisited = [[0, 0]];
+    let currentPositionSanta = [0, 0];
+    let currentPositionRobot = [0, 0]
+
+    const arrayIncluded = (arrayCompared, setOfArray) => {
+        for (let array of setOfArray) {
+            if (array[0] === arrayCompared[0] && array[1] === arrayCompared[1] ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    for (let index = 0; index < puzzle.length; index++) {
+        if (index%2 === 0) {
+            currentPositionSanta = calculateNewPosition((puzzle[index]), currentPositionSanta);
+            if ( !arrayIncluded(currentPositionSanta, positionsVisited) ) {
+                positionsVisited.push(currentPositionSanta);
+            }
+        } else {
+            currentPositionRobot = calculateNewPosition((puzzle[index]), currentPositionRobot);
+            if ( !arrayIncluded(currentPositionRobot, positionsVisited) ) {
+                positionsVisited.push(currentPositionRobot);
+            }
+        }
+    }
+    return positionsVisited.length;
+}
+
+console.log('Part 1 : ', solvePuzzle1());
+console.log('Part 2 : ', solvePuzzle2());
