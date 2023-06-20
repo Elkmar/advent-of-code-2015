7716285bf2e1c0d77be419773bb581801667829a
Elkmar <dimitri.jeangerard@gmail.com>
2023-06-20 09:55:22 +0100

    Solution Part 2 + hash function

--- Day 4/script.js
+++ Day 4/script.js
@@ -3,12 +3,25 @@ const CryptoJS = require("crypto-js");
 const data = fs.readFileSync('puzzle.txt');
 const puzzle = data.toString();
 
+const calculateHash = (prefix, i) => {
+    return CryptoJS.MD5(prefix+i.toString()).toString(CryptoJS.enc.Hex);
+}
+
 const solvePuzzle1 = () => {
     let i = 0;
-    while (CryptoJS.MD5(puzzle+i.toString()).toString(CryptoJS.enc.Hex).substring(0, 5) != '00000') {
+    while (calculateHash(puzzle, i).substring(0, 5) != '00000') {
+        i++;
+    }
+    return i;
+}
+
+const solvePuzzle2 = () => {
+    let i = 0;
+    while (calculateHash(puzzle, i).substring(0, 6) != '000000') {
         i++;
     }
     return i;
 }
 
 console.log('Part 1 : ', solvePuzzle1());
+console.log('Part 2 : ', solvePuzzle2());
