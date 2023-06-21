ee27f211de1447e1365be09086ed338141aa2b9f
Elkmar <dimitri.jeangerard@gmail.com>
2023-06-21 23:17:02 +0100

    functions cleaned

--- Day 5/script.js
+++ Day 5/script.js
@@ -3,51 +3,53 @@ const data = fs.readFileSync('puzzle.txt');
 const puzzle = data.toString();
 const arrayOfStrings = puzzle.split(/\r?\n/);
 
-const check3diffVowels = (string) => {
-    let vowelsFound = '';
-    for (let char of string) {
-        for (let vowel of "aeiou") {
-            if (char === vowel) {
-                vowelsFound += char;
+const solvePuzzle1 = () => {
+
+    const check3diffVowels = (string) => {
+        let vowelsFound = '';
+        for (let char of string) {
+            for (let vowel of "aeiou") {
+                if (char === vowel) {
+                    vowelsFound += char;
+                }
             }
         }
+    
+        if (vowelsFound.length > 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    if (vowelsFound.length > 2) {
-        return true;
-    } else {
+    
+    const checkDoubleLetter = (string) => {
+        let prevChar = undefined;
+        for (let char of string) {
+            if (prevChar === char) {
+                return true;
+            }
+            prevChar = char;
+        }
         return false;
     }
-}
-
-const checkDoubleLetter = (string) => {
-    let prevChar = undefined;
-    for (let char of string) {
-        if (prevChar === char) {
-            return true;
+    
+    const checkProhibitedStrings = (string) => {
+        let subStrings = [];
+        let prohibitedStrings = ['ab', 'cd', 'pq', 'xy'];
+        for (let i = 0; i < (string.length - 1); i++) {
+            let substring = string.substring(i, i+2)
+            subStrings.push(substring);
         }
-        prevChar = char;
-    }
-    return false;
-}
-
-const checkProhibitedStrings = (string) => {
-    let subStrings = [];
-    let prohibitedStrings = ['ab', 'cd', 'pq', 'xy'];
-    for (let i = 0; i < (string.length - 1); i++) {
-        let substring = string.substring(i, i+2)
-        subStrings.push(substring);
-    }
-    for (let prohibitedString of prohibitedStrings) {
-        if (subStrings.includes(prohibitedString)) {
-            return true;
+        for (let prohibitedString of prohibitedStrings) {
+            if (subStrings.includes(prohibitedString)) {
+                return true;
+            }
         }
+        return false;
     }
-    return false;
-}
 
-const solvePuzzle1 = () => {
     let niceStrings = 0;
+    
     for (string of arrayOfStrings) {
         if (check3diffVowels(string) && checkDoubleLetter(string) && !checkProhibitedStrings(string)) {
             niceStrings ++;
