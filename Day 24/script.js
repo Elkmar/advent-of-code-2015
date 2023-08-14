5dbb310a300006df4d98d045f254a4be2471407f
Elkmar <dimitri.jeangerard@gmail.com>
2023-08-14 14:47:02 +0100

    Part 2 solved, few minutes required

--- Day 24/script.js
+++ Day 24/script.js
@@ -3,9 +3,8 @@ const puzzle =  fs.readFileSync('./puzzle.txt', 'utf-8');
 const weightsArray = puzzle.split('\n').map((weight) => parseInt(weight));
 const weightsDescendingOrder = weightsArray.sort((a, b) => b - a);
 
-//Calculate the quantum Entanglement of the first group when the best combination is found
-const quantumEntanglementBestCombination = (weights) => {
-    debugger;
+//Calculate the quantum Entanglement of the first group when the best combination is found, with 3 groups
+const quantumEntanglementBestCombination1 = (weights) => {
 
     let bestCombination = null;
 
@@ -22,8 +21,6 @@ const quantumEntanglementBestCombination = (weights) => {
     }
 
     const distributeWeights = (index, group1, group2, group3) => {
-
-        debugger;
 
         // If we've reached the end of the weights array, we can stop and check if we have a new best combination
         if (index === weights.length) {
@@ -65,4 +62,72 @@ const quantumEntanglementBestCombination = (weights) => {
     return calculateQuantumEntanglement(bestCombination);
 }
 
-console.log('Answer 1: ', quantumEntanglementBestCombination(weightsDescendingOrder));
+console.log('Answer 1: ', quantumEntanglementBestCombination1(weightsDescendingOrder));
+
+//Calculate the quantum Entanglement of the first group when the best combination is found, with 4 groups
+const quantumEntanglementBestCombination2 = (weights) => {
+
+    let bestCombination = null;
+
+    // Calculate the weight of a group of weights
+    const calculateWeight = (group) => {
+        return group.reduce((acc, weight) => acc + weight, 0);
+    }
+
+    let totalWeight = calculateWeight(weights);
+
+    // Calculate the quantum entanglement of a group of weights
+    const calculateQuantumEntanglement = (group) => {
+        return group.reduce((acc, weight) => acc * weight, 1);
+    }
+
+    const distributeWeights = (index, group1, group2, group3, group4) => {
+
+        // If we've reached the end of the weights array, we can stop and check if we have a new best combination
+        if (index === weights.length) {
+          if (calculateWeight(group1) === calculateWeight(group2) &&
+              calculateWeight(group2) === calculateWeight(group3) && 
+              calculateWeight(group3) === calculateWeight(group4) &&
+              (bestCombination === null || group1.length < bestCombination.length || (group1.length === bestCombination.length &&
+                calculateQuantumEntanglement(group1) < calculateQuantumEntanglement(bestCombination)))) {
+                    bestCombination = group1.slice();
+                    console.log('bestCombination', bestCombination);
+                }
+          return;
+        }
+
+        // If the current group is already heavier than the target weight, we can stop
+        if (calculateWeight(group1) > totalWeight / 4 ||
+            calculateWeight(group2) > totalWeight / 4 ||
+            calculateWeight(group3) > totalWeight / 4 || 
+            calculateWeight(group4) > totalWeight / 4) {
+          return;
+        }
+
+        // If we already have a best combination and the current group is longer than it, we can stop
+        if (bestCombination !== null && (group1.length > bestCombination.length || calculateQuantumEntanglement(group1) > calculateQuantumEntanglement(bestCombination))) {
+            return;
+        }
+
+        group1.push(weights[index]);
+        distributeWeights(index + 1, group1, group2, group3, group4);
+        group1.pop();
+
+        group2.push(weights[index]);
+        distributeWeights(index + 1, group1, group2, group3, group4);
+        group2.pop();
+
+        group3.push(weights[index]);
+        distributeWeights(index + 1, group1, group2, group3, group4);
+        group3.pop();
+
+        group4.push(weights[index]);
+        distributeWeights(index + 1, group1, group2, group3, group4);
+        group4.pop();
+    }
+
+    distributeWeights(0, [], [], [], []);
+    return calculateQuantumEntanglement(bestCombination);
+}
+
+console.log('Answer 2: ', quantumEntanglementBestCombination2(weightsDescendingOrder));
